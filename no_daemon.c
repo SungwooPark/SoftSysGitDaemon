66736a0dde3180c497377833a29962bd80d96879
Sung Park <sungwoo.park@students.olin.edu>
2018-03-15 13:42:14 -0400

    Implement last commit time monitor

--- no_daemon.c
+++ no_daemon.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 //Return 1 if string pre is a prefix of string str. 0 otherwise
 int prefix(const char *pre, const char *str)
@@ -9,7 +10,8 @@ int prefix(const char *pre, const char *str)
     return strncmp(pre, str, strlen(pre)) == 0;
 }
 
-int main(){
+//Return time in second since last commit
+double time_since_last_commit(){
   FILE *fp;
   char output[1035];
   struct tm tm;
@@ -27,19 +29,29 @@ int main(){
   while (fgets(output, sizeof(output)-1, fp) != NULL) {
     // printf("%s", output);
     if (prefix("Date", output)){
-      printf("%s", output);
       if (strptime(output, "Date:   %a %b %d %H:%M:%S %Y", &tm) != NULL){
         commit_time = mktime(&tm);
         time(&current_time);
-        printf("commit diff time: %f\n", difftime(current_time, commit_time));
+        return difftime(current_time, commit_time);
       } else {
         printf("%s", "something went wrong during time extraction");
+        return -1;
       }
     }
   }
 
   /* close */
   pclose(fp);
+}
 
+int main(){
+  while(1){
+    double last_commit_time_diff = time_since_last_commit();
+    printf("seconds since last commit: %f\n", last_commit_time_diff);
+    if (last_commit_time_diff > 100.0){
+      printf("You have not commited your work in while. Commit your work!\n");
+    }
+    sleep(10);
+  }
   return 0;
 }
